6579e69a1d0afac7c9432d5cbb52551b7b7af055
Jake Buroker <shamrecks@gmail.com>
2023-11-26 17:45:30 -0600

    completed

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,11 +1,49 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
+let myCollection = [];
+
+function addToCollection(collection, title, artist, yearPublished) {
+  let newAlbum = {
+      title: title,
+      artist: artist,
+      yearPublished: yearPublished
+  };
+  collection.push(newAlbum);
+  return newAlbum;
+}
 
 
+let newAlbum1 = addToCollection(myCollection, "Good A:M", "Mac Miller","2015" );
+console.log(myCollection);
+let newAlbum2 = addToCollection(myCollection, "Graduation", "Kanye West","2007" );
+let newAlbum3 = addToCollection(myCollection, "The Marshall Mathers LP", "Eminem","2000" );
+let newAlbum4 = addToCollection(myCollection, "Madvillainy", "Madvillain","2004" );
+let newAlbum5 = addToCollection(myCollection, "Pray For Paris", "Westside Gunn","2020" );
+let newAlbum6 = addToCollection(myCollection, "Nothing Great About Britain", "slowthai","2019" );
+console.log(myCollection);
 
+function showCollection(collection) {
+for (let i = 0; i < collection.length; i++) {
+  let item = collection[i];
+  console.log(`Item ${i + 1}: Title: ${item.title}, Artist: ${item.artist}, Year Published: ${item.yearPublished}`);
+}
+}
+showCollection(myCollection);
 
+function findByArtist(collection, artist) {
+  let results = [];
 
+  for (let i = 0; i < collection.length; i++) {
+      if (collection[i].artist === artist) {
+          results.push(collection[i]);
+      }
+  }
 
+  return results;
+}
+
+let EminemAlbums = findByArtist(myCollection, 'Eminem');
+console.log(EminemAlbums);
 
 
 
